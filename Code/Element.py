adf196117a0d0047d1dc27362298247d5c171733
yy18-sys <53955330+yy18-sys@users.noreply.github.com>
2020-12-27 21:21:07 +0100

    Updated Elements

--- Code/Element.py
+++ Code/Element.py
@@ -5,21 +5,3 @@ class Element:
 
     def getname(self):
         return self.name
-
-class Ground(Element):
-
-
-    def isGround(self):
-        return  True
-
-class Food(Element):
-    def __init__(self):
-        super.__init__(self)
-
-class Lava(Element):
-    def __init__(self):
-        super.__init__(self)
-
-class Wall(Element):
-    def __init__(self):
-        super.__init__(self)
