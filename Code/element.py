af8c34c447ae093ceb68eb23f3b59c2dea0c1f31
yy18-sys <53955330+yy18-sys@users.noreply.github.com>
2020-12-27 21:39:27 +0100

    Create human.py

--- Code/element.py
+++ Code/element.py
@@ -3,6 +3,12 @@ class Element:
     def __init__(self,state):
         self.state = state
 
-    def getState(self):
-        if(self.state == 0):
+    def getAction(self):
+        if self.state == 0:
             print("ground")
+        elif self.state == 1:
+            print("food")
+        elif self.state == 2:
+            print("wall")
+        elif self.state == 3:
+            print("lava")
---
+++ Code/human.py
@@ -0,0 +1,29 @@
+class human:
+
+    def __init__(self, name,maxhealth,health,maxfood,food):
+        self.name = name
+        self.maxhealth = maxhealth
+        self.health = health
+        self.maxfood = maxfood
+        self.food = food
+        self.alive = True
+
+    def setDamage(self, damage):
+        currentHealth =  self.health - damage
+        if currentHealth <= 0:
+            self.alive = False
+        else:
+            if currentHealth > self.maxhealth:
+                self.health = self.maxhealth
+            else:
+                self.health = currentHealth
+
+    def addFood(self, food):
+        currentFood = self.food - food
+        if currentFood <= 0:
+            self.alive = False
+        else:
+            if currentFood > self.maxfood:
+                self.food = self.maxfood
+            else:
+                self.food = currentFood
